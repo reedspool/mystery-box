2c6b87a0ad9b4c0a9d713b87c658d192fc60cd67
reedspool <splinterspool@yahoo.com>
2014-07-25 01:45:47 -0400

    Working mystery box

--- app/scripts/driver.js
+++ app/scripts/driver.js
@@ -1,6 +1,20 @@
 /* Driver.js: Run our web app! */
-define(['d3', 'jquery', 'lodash', 'jquery.eventAggregator', 'knockout'],
+define(['d3', 'jquery', 'lodash', 'jquery.eventAggregator', 'knockout', 'typed'],
   function(d3, $, _, eventAggregator, ko) {
-    // Start here!
+    var quote = [ "There is something unspeakably sinister about a machine that does nothing  " +
+    			  "^2000 -- absolutely nothing --  "
+    			+ "^1000 except switch itself off.  "
+    			+ "^300 -- Arthur C. Clarke ^2000 "];
+
+    function goBlank() { window.location = "about:blank"; };
+
+    $(function(){
+      $(".typewriter").typed({
+        strings: quote,
+        typeSpeed: 30,
+        callback: goBlank
+      });
+  });
+
   }
 );
